d139317e1a4c8e11bb500dd02e25d6a704225d9d
leedah <ledazach@gmail.com>
2017-06-01 07:30:49 +0300

    Rename main.py to visualization.py, change colors

--- visualization.py
+++ visualization.py
@@ -1,14 +1,16 @@
-
 import pandas as pd
-
 import random
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_extraction import DictVectorizer as DV
 
 print "*****************  DATA LOADING *********************"
+
 df = pd.read_csv('./dataSets/train.tsv', sep='\t', header=0)
 
-# Removing Id feature because a sequential feature like this is not good.
+# Removing Id feature because a sequential feature like this is not good
+
 df = df.drop('Id', 1)
 
 x=df.loc[df['Label'] == 2]
@@ -19,17 +21,24 @@ def is_categorical(array_like):
 
 sns.set(color_codes=True)
 
-print "********************   GENERATE HISTOGRAMS AND BOX PLOTS *******************"
+print "******** GENERATE HISTOGRAMS AND BOX PLOTS **********"
+
 # Looping on features excluding the 'Label' field
+
 for i, column in enumerate(df.drop('Label', 1)):
     if(is_categorical(df[column])):
         fig=sns.plt.figure(i)
-        g = sns.countplot(y=column, hue="Label", data=df, palette="Reds_d");
+        g = sns.countplot(y=column, hue="Label", data=df, palette="Set2");
         g.axes.set_title("Histogram for " + column, fontsize=24,alpha=0.5)
         fig.savefig('output/histograms/'+ column);
     else:
         fig=sns.plt.figure(i)
-        sns.boxplot(y=column, x="Label", data=df);
-        g = sns.boxplot(y=column, x="Label", data=df);
+        g = sns.boxplot(y=column, x="Label", data=df, palette="Set2");
         g.axes.set_title("Box plot for " + column, fontsize=24,alpha=0.5)
         fig.savefig('output/boxPlots/'+ column);
+
+
+
+
+
+
